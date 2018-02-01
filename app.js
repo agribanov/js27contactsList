2bca4e74065358de95caf186edef53d84df5b049
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2018-02-02 00:24:25 +0200

    26 module updates

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 $(function(){
-	var contactsList = [];
+	var contactsList = {};
 
 	var $formInputs = $('.form-input');
+	var $id = $('#id');
 	var $name = $('#name');
 	var $surname = $('#surname');
 	var $submitBtn = $('#submit');
+	var $clearBtn = $('#clearFormBtn')
 	var $form = $('form');
+	var $showFormBtn = $('#showFormBtn');
 	var $contactsTable = $('table');
 
 	var contactTemplate = $('#contactTemplate').html();
@@ -14,11 +17,33 @@ $(function(){
 		.change(onFormChange)
 		.submit(onFormSubmit)
 
-	$contactsTable.on('click', '.delete-btn', onDeleteBtnClick)
+	$clearBtn.click(onClearBtnClick)
 
+	$contactsTable
+		.on('click', '.delete-btn', onDeleteBtnClick)
+		.on('click', '.edit-btn', onEditBtnClick)
+
+	$showFormBtn.click('click', onShowFormBtnClick)
 	init();
 
 	// Callbacks
+	function onClearBtnClick(event){
+		event.preventDefault();
+		clearForm();
+	}
+
+	
+
+	function onShowFormBtnClick(){
+		$form.toggleClass('collapsed');
+	}
+
+	function onEditBtnClick(){
+		var id = $(this).closest('tr').data('id');
+
+		editContact(id)	
+	}
+
 	function onDeleteBtnClick(){
 		var id = $(this).closest('tr').data('id');
 
@@ -33,25 +58,50 @@ $(function(){
 		event.preventDefault();
 
 		var contact = {
+			id: $id.val(),
 			name: $name.val(),
 			surname: $surname.val()
 		}
 
-		addContact(contact);
+		setContact(contact);
+		clearForm();
 	}
 
 	// DOM Manipulations
-	function renderNewContact(contact){
-		var row = contactTemplate
-				.replace('{{id}}', contact.id)
-				.replace('{{name}}', contact.name)
-				.replace('{{surname}}', contact.surname);
+	function getContactElemet(id){
+		return $('[data-id="' + id + '"]');
+	}
 
-		$contactsTable.append(row);
+	function createContactElement(contact){
+		var row = contactTemplate
+			.replace('{{id}}', contact.id)
+			.replace('{{name}}', contact.name)
+			.replace('{{surname}}', contact.surname);
+
+		return $(row);
+	}
+
+	function renderNewContact(contact){
+		var $row = createContactElement(contact);
+
+		$contactsTable.append($row);
+
+		$row.fadeIn(300)
 	}
 
 	function removeContactFromTable(id){
-		$('[data-id="' + id + '"]').remove();
+		var $tr = getContactElemet(id);
+
+		$tr.fadeOut('slow', function(){
+			$tr.remove()
+		});
+	}
+
+	function renderUpdatedContact(contact){
+		var $tr = getContactElemet(contact.id);
+		var $row = createContactElement(contact);
+		console.log('rendering updated', $tr, $row)
+		$tr.replaceWith($row);
 	}
 
 	function enableButton(){
@@ -60,6 +110,20 @@ $(function(){
 
 	function disableButton(){
 		$submitBtn.attr('disabled', 'disabled')
+	}
+
+	function clearForm(){
+		$id.val('');
+		$name.val('');
+		$surname.val('');
+	}
+
+	function fillForm(contact){
+		$id.val(contact.id);
+		$name.val(contact.name);
+		$surname.val(contact.surname);
+
+		checkFormValid();
 	}
 
 	// Bussiness Logic
@@ -75,24 +139,37 @@ $(function(){
 		});
 	}
 
-	function addContact(contact){
-		contact.id = Date.now();
 
-		contactsList.push(contact);
-
-		renderNewContact(contact);
+	function setContact(contact){
+		contact.id ? updateContact(contact) : addContact(contact);
 		saveContacts();
 	}
 
+	function addContact(contact){
+		contact.id = Date.now();
+		contactsList[contact.id] = contact; // {2 : {id: 2, name: 'John', surname: 'Doe'}}
+
+		renderNewContact(contact);
+	}
+
+	function updateContact(contact){
+		contactsList[contact.id] = contact; 
+
+		renderUpdatedContact(contact);
+	}
 
 	function deleteContact(id){
-		contactsList = contactsList.filter(function(contact){
-			return contact.id != id;
-		});
+		delete contactsList[id];
 
 		saveContacts();
 
 		removeContactFromTable(id)
+	}
+
+	function editContact(id){
+		var contact = contactsList[id];
+
+		fillForm(contact);
 	}
 
 	function saveContacts(){
